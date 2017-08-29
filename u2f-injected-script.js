6c684f086777315bde877dbb79a3a210313b3c58
btidor <btidor@mit.edu>
2017-08-28 23:33:02 -0700

    Switch to more robust injection mechanism

--- u2f-injected-script.js
+++ u2f-injected-script.js
@@ -1,23 +1,121 @@
-'use strict';
+(function() {
+  'use strict';
 
-// Rather than take the risk of running a lot of code in a foreign context,
-// we're going to selectively target the Google-provided U2F polyfill, which
-// most sites use. By overriding the polyfill's EXTENSION_ID variable, we can
-// re-use the polyfill's logic (the iFrame trampoline in particular) to
-// communicate with our extension.
+  const OLD_EXTENSION_ID = 'kmendfapggjehodndflmmgagdbamhnfd';
+  const NEW_EXTENSION_ID = 'jnhbfcokdhpfagldfpiaficnpnadkoma';
 
-// Make sure window.u2f is defined
-var u2f = u2f || {};
+  const getIframePort = function(callback) {
+    // Create the iframe
+    var iframeOrigin = 'chrome-extension://' + NEW_EXTENSION_ID;
+    var iframe = document.createElement('iframe');
+    iframe.src = iframeOrigin + '/u2f-comms.html';
+    iframe.setAttribute('style', 'display:none');
+    document.body.appendChild(iframe);
 
-// Define EXTENSION_ID as a read-only property. Further attempts to assign a
-// value to this field will be a no-op, and will not raise an error. Note that
-// the polyfill uses strict mode, so assigning to a writable: false property
-// would raise an error.
-Object.defineProperty(u2f, 'EXTENSION_ID', {
-  get: function() { return 'jnhbfcokdhpfagldfpiaficnpnadkoma'; },
-  set: function(v) { return v; },
-});
+    var channel = new MessageChannel();
+    var ready = function(message) {
+      if (message.data == 'ready') {
+        channel.port1.removeEventListener('message', ready);
+        callback(channel.port1);
+      } else {
+        console.error('[u2f-touchid] First event on iframe port was not "ready"');
+      }
+    };
+    channel.port1.addEventListener('message', ready);
+    channel.port1.start();
 
-// Reset the polfyill's internal state, causing it to re-run port selection with
-// our new EXTENSION_ID.
-if (typeof u2f.port_ !== 'undefined') u2f.port_ = null;
+    iframe.addEventListener('load', function() {
+      // Deliver the port to the iframe and initialize
+      iframe.contentWindow.postMessage('init', iframeOrigin, [channel.port2]);
+    });
+  };
+
+  const inject = function(port) {
+    // Multiplex requests and responses across the port.
+    var reqId = -1;
+    var outstanding = {};
+    const sendMessageWithCallback = function(message_, callback) {
+      var message = Object.assign({}, message_);
+      outstanding[reqId] = {
+        originalId: message['requestId'],
+        callback: callback,
+      }
+      message['requestId'] = reqId;
+      reqId = reqId - 1;
+      port.postMessage(message);
+    }
+
+    port.addEventListener('message', function(message) {
+      console.warn(message);
+      const data = message.data;
+      const reqId = data['requestId'];
+      if (!reqId || !outstanding[reqId]) {
+        console.error('[u2f-touchid] Unknown or missing requestId in response.');
+        return;
+      }
+      data['requestId'] = outstanding[reqId].originalId;
+      const callback = outstanding[reqId].callback;
+      delete outstanding[reqId];
+      callback(data);
+    });
+
+    // Intercept calls to sendMessage(OLD_EXTENSION_ID, ...) and proxy them to
+    // our extension.
+    const sendMessage_ = chrome.runtime.sendMessage;
+    chrome.runtime.sendMessage = function(...args) {
+      console.warn(["sendMessage(", ...args]);
+
+      if (args.length >= 2 && args[0] == OLD_EXTENSION_ID) {
+        const message = args[1];
+        const callback = args.slice(-1)[0];
+        if (!(callback instanceof Function)) {
+          callback = function(message) {};
+        }
+
+        sendMessageWithCallback(message, callback);
+        return;
+      } else {
+        return sendMessage_(...args);
+      }
+    };
+
+    // Intercept calls to connect(OLD_EXTENSION_ID, ..) and proxy them to our
+    // extension.
+    const connect_ = chrome.runtime.connect;
+    chrome.runtime.connect = function(...args) {
+      console.warn(["connect(", ...args]);
+
+      if (args.length > 1 && args[0] == OLD_EXTENSION_ID) {
+        var listeners = [];
+        // TODO: use `prototype` instead?
+        var fakePort = {
+          name: OLD_EXTENSION_ID,
+          disconnect: function() {
+            fakePort.close();
+          },
+          onDisconnect: {
+            // not supported
+            addListener: function(listener) {},
+          },
+          onMessage: {
+            addListener: function(listener) {
+              listeners.push(listener);
+            },
+          },
+          postMessage: function(message) {
+            sendMessageWithCallback(message, function(response) {
+              listeners.forEach(function(listener) {
+                listener(response);
+              });
+            });
+          },
+        }
+        return fakePort;
+      } else {
+        return connect_(...args);
+      }
+    };
+  }
+
+  getIframePort(inject);
+}());
